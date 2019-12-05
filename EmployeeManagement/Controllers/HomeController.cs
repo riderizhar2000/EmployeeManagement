3f49b53e465cb610aeeaf013bbf2e7f96279a7bc
Syed Izharullah <riderizhar2000@gmail.com>
2019-12-05 23:40:36 +0530

    Model Validation

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -42,10 +42,15 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpPost]
-        public RedirectToActionResult Create(Employee employee)
+        public IActionResult Create(Employee employee)
         {
-            Employee newEmployee = _employeeRepository.Add(employee);
-            return RedirectToAction("Details", "Home", new { id = employee.Id });
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("Details", "Home", new { id = employee.Id });
+            }
+
+            return View();
         }
     }
 }
--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,16 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$", ErrorMessage = "Invalid Email Format")]
+        [Display(Name = "Email Id")]
         public string Email { get; set; }
+
         public Dept Department { get; set; }
     }
 }
