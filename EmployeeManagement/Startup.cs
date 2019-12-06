7ed5935c9f847f1bb26ff64763bb7f7ff91a07d4
Syed Izharullah <riderizhar2000@gmail.com>
2019-12-06 12:23:42 +0530

    AddSingleton() vs AddScoped() vs AddTransient()

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -27,7 +27,7 @@ namespace EmployeeManagement
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().AddXmlSerializerFormatters();
-            services.AddSingleton<IEmployeeRepository, EmployeeRepository>();
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
