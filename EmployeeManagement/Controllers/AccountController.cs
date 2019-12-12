84d102f232189df709283b3d2a22ab3516ab7745
Syed Izharullah <riderizhar2000@gmail.com>
2019-12-12 13:25:36 +0530

    Register New User

---
+++ EmployeeManagement/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
